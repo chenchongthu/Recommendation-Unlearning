d2fba1f4a136d24ff7c5c7063a3b2f662050426a
Chong Chen <cstchenc@163.com>
2022-01-22 13:57:13 +0800

    Delete code directory

--- code/evaluator/__init__.py
+++
@@ -1,8 +0,0 @@
-# import eval_score_matrix_foldout
-try:
-    from evaluator.cpp.evaluate_foldout import eval_score_matrix_foldout
-    print("eval_score_matrix_foldout with cpp")
-except:
-    from evaluator.python.evaluate_foldout import eval_score_matrix_foldout
-    print("eval_score_matrix_foldout with python")
-    
